aedf9985188b6d6b47477a64584a9b9147ea1d01
nikkii94 <k.nikolett17@gmail.com>
2020-03-18 19:54:21 +0100

    Updates

---
+++ precache-manifest.c8fb248b52d3c1b323cbe61c9682d0f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23ae5026945199210401a4f63ae11480",
+    "url": "/learning-react/index.html"
+  },
+  {
+    "revision": "422b7f1c63bc48b7e72d",
+    "url": "/learning-react/static/css/main.a2e4e351.chunk.css"
+  },
+  {
+    "revision": "969920d439d325d1251a",
+    "url": "/learning-react/static/js/2.820d8c15.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/learning-react/static/js/2.820d8c15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "422b7f1c63bc48b7e72d",
+    "url": "/learning-react/static/js/main.e69d8941.chunk.js"
+  },
+  {
+    "revision": "8a52c365d5bd8b0b7d0d",
+    "url": "/learning-react/static/js/runtime-main.1efb8146.js"
+  }
+]);
